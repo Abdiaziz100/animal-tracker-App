510bd78d27727cfae0a787c30e30de228c085dd9
Abdiaziz100 <abdiazizmahat100@gmail.com>
2026-05-02 11:56:22 +0300

    🌍 Switch to production API

--- mobile/config.js
+++ mobile/config.js
@@ -1,5 +1,2 @@
-// Change this to your deployed URL after deployment
-// Local:      'http://192.168.0.108:5000/api'
-// Production: 'https://animal-tracker-backend.onrender.com/api'
-
-export const API_URL = 'http://192.168.0.108:5000/api';
+// Production URL - Deployed on Render.com
+export const API_URL = 'https://animal-tracker-app.onrender.com/api';
