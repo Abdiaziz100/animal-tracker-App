d77002e8ab5b73ca34d3fa51ffbebbb146b2f545
Abdiaziz100 <abdiazizmahat100@gmail.com>
2026-06-10 14:25:47 +0300

    Update API URL to new Render deployment

--- animal-tracker-rn/mobile/config.js
+++ animal-tracker-rn/mobile/config.js
@@ -1,2 +1,2 @@
 // Production URL - PostgreSQL Database
-export const API_URL = 'https://animal-tracker-app-6.onrender.com/api';
+export const API_URL = 'https://animal-tracker-app-7.onrender.com/api';
